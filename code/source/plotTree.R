3b4f87fb1574490401858f2b8d1dc98aa60edc25
silke <s81320@beuth-hochschule.de>
2022-01-06 21:31:49 +0100

    works on iris

--- code/source/plotTree.R
+++ code/source/plotTree.R
@@ -14,36 +14,72 @@ plotTree <- function(rg,tri,...){
   
   # remove terminal nodes
   ti <- ti[!ti$terminal,]
-
-  names(ti) <-  NULL
-
-  el <- rbind(as.matrix(ti[,c(1,2)]) , as.matrix(ti[,c(1,3)]))
-  el <-  el+1 # originally nodeID in ranger starts at 0 but we need positive numbers for the nodes
-
+  ti
+  ti$splitvarName %>% unique -> d0.info
+  
+  sn <- unique(ti[,c('nodeID','splitvarID','splitvarName')]) # the split nodes by ID
+  sn
+  el <- matrix(NA,2*nrow(ti),ncol=2)
+  ct <-  1
+  for(i in 1:nrow(ti)){
+    if(ti[i,'leftChild'] %in% sn$nodeID ){
+      #print(paste('add row' , i, ct))
+      el[ct,] <- ti[i,c('nodeID','leftChild')] %>% unlist # add edge
+      ct <- ct+1
+    }
+    if(ti[i,'rightChild'] %in% sn$nodeID ){
+      el[ct,] <- ti[i,c('nodeID','rightChild')] %>% unlist # add edge
+      ct <- ct+1
+    }
+  }
+  
+  el <- el[!is.na(el[,1]),]
+  #el %>% print
+  
+  el <- el+1 # originally nodeID in ranger starts at 0 but we need positive numbers for the nodes
+  sn$nodeID <-  sn$nodeID+1
+  el # nodes need to be numbered 1,2,3.. no gaps allowed
+  # splitvarName has to be tied to the numbering of nodes
+  
+  ist <- unique(as.vector(el))
+  ist <-  ist[order(ist)]
+  soll <- 1:length(ist)
+  
+  for(i in soll){
+    el[el==ist[i]] <- soll[i]
+  }
+  
+  sn$nodeID <- soll
+  
   graph_from_edgelist(el) -> g1
 
   lo <-  layout_as_tree(g1,root=1)
   
+  # g1 %>% plot(layout=lo, main=tri, mode='out') # simpler plot
   g1 %>% plot(layout=lo
-              , vertex.label.cex=0.7
-            , vertex.label=ti[,5] # ti$splitvarName
-            , vertex.color=NA
-            , vertex.frame.color=NA
+            , vertex.label.cex=0.7
+            , vertex.label=sn$splitvarName # needs to be in sync with edge list el
+            #, vertex.color=NA
+            #, vertex.frame.color=NA
             #, vertex.size=50
-            #, vertex.shape='none'
+            , vertex.shape='none'
             #, vertex.label.dist=0.5 # dist from the center of the vertex
             , edge.color ='red'
-          , rescale = T
+            , edge.arrow.size=.4
+            , rescale = T
           # , ylim=0.5*range(lo[,2])
           # ,xlim=0.5*range(lo[,1])
           , asp = 0
           , ...
           )
+  
+  return(d0.info)
 }
 
+rg <- ranger(Species~. , num.trees=3 , data=iris , max.depth = 4)
 
+treeInfo(rg,2)
 
-rg <- ranger(Species~. , num.trees=3 , data=iris)
 plotTree(rg, 1, main='tree 1')
 plotTree(rg, 2, main='tree 2')
 plotTree(rg, 3, main='tree 3')
@@ -55,6 +91,15 @@ createDMd1(rg$forest, iris)
 createDMd1(rg$forest, df)
 
 createDMsb(rg$forest)
+
+rt2fbt(rg$forest,1)
+
+# full binary trees built from ranger trees
+lapply(1:rg$forest$num.trees, function(i) rt2fbt(rg$forest,i) ) ->
+  A
+A
+
+treeInfo(rg,1)
 
 load('data/data_SupMat4.rda')
 
